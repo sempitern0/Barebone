348ab15a27495a92ab27e71829855388352f936b
sempitern0 <danieldigitalspace@proton.me>
2025-10-04 12:28:20 +0100

    linking placeable manager and placeables to have a correct flow

--- components/systems/build/placeable/placeable_3d.gd
+++ components/systems/build/placeable/placeable_3d.gd
@@ -6,7 +6,9 @@ class_name Placeable3D extends Node3D
 @export_multiline var description: String
 @export var target: Node3D
 @export var placement_area: PlacementArea3D
-@export var height_offset: float = 0.0
+## Internal collisionable objects to be excluded on the ray query to drag in the world
+@export var collisionables: Array[CollisionObject3D] = []
+@export var offset: Vector3 = Vector3.ZERO
 @export var drag_mode: DragMode = DragMode.Manual
 @export_category("Camera")
 @export var origin_camera: Camera3D
@@ -37,3 +39,28 @@ func _ready() -> void:
 		
 	set_process_unhandled_input(placing)
 	set_physics_process(placing)
+
+	handle_drag_motion()
+
+	
+func handle_drag_motion():
+	if origin_camera and placing:
+		var mouse_position: Vector2 = get_viewport().get_mouse_position()
+		
+		var world_space: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
+		var from: Vector3 = origin_camera.project_ray_origin(mouse_position)
+		var to: Vector3 = origin_camera.project_position(mouse_position, drag_distance_from_camera)
+		
+		var ray_query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(from, to)
+		ray_query.exclude = collisionables.map(
+			func(collisionable: CollisionObject3D): return collisionable.get_rid()
+		)
+	
+		ray_query.collide_with_areas = false
+		ray_query.collide_with_bodies = true
+		
+		var result: Dictionary = world_space.intersect_ray(ray_query)
+	
+		if result.has("position"):
+			target.global_position = result.position
+			target.global_position += offset
--- components/systems/build/placeable/placeable_manager.gd
+++ components/systems/build/placeable/placeable_manager.gd
@@ -2,25 +2,38 @@ class_name PlaceableManager extends Node
 
 ## The root node to instance the selected placeables
 @export var world: Node
+## The origin camera to use for placing objects when the placeable does not have one assigned
+@export var origin_camera: Camera3D
 
 ## Placeable Ids
 ## Example: const Fridge: StringName = &"fridge"
-
 const TestFurniture: StringName = &"test"
-
 
 var placeables: Dictionary[StringName, PackedScene] = {
 	TestFurniture: preload("uid://c27as6ww6qr2g")
 }
 
 
+var current_placeable: Placeable3D
+
+
 func _ready() -> void:
 	if world == null:
 		world = get_tree().root
+		
+	assert(world is Node3D, "PlaceableManager: The world node to place objects needs to inherit from Node3D")
 
 
 func spawn(id: StringName) -> void:
-	if placeables.has(id):
-		var placeable: Placeable3D = placeables[id].instantiate() as Placeable3D
+	if not placeables.has(id):
+		push_warning("PlaceableManager: The placeable with id %s does not exists or is not assigned to a Placeable3D scene" % id)
+		return
 		
-		world.add_child(placeable)
+	var placeable: Placeable3D = placeables[id].instantiate() as Placeable3D
+	
+	if placeable.origin_camera == null:
+		placeable.origin_camera = origin_camera
+		
+	world.add_child(placeable)
+	placeable.placing = true
+	current_placeable = placeable
