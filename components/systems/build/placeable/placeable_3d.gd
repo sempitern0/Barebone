01282e4af377cb95aa497c5fb0d8c853e6f3b4d4
sempitern0 <danieldigitalspace@proton.me>
2025-10-04 11:10:03 +0100

    prototype texture checker and started build system to place 3D objects in the world

---
+++ components/systems/build/placeable/placeable_3d.gd
@@ -0,0 +1,36 @@
+@icon("res://components/systems/build/placeable/placeable.svg")
+class_name Placeable3D extends Node3D
+
+@export var target: Node3D
+@export var placement_area: PlacementArea3D
+@export var height_offset: float = 0.0
+@export var drag_mode: DragMode = DragMode.Manual
+@export_category("Camera")
+@export var origin_camera: Camera3D
+@export var drag_distance_from_camera: float = 50.0
+
+enum DragMode {
+	ClickToDrag, ## Keep pressed the collisionable to drag it
+	Manual ## Change manually via code the "placing" variable to activate the drag
+}
+
+var placing: bool = false:
+	set(value):
+		if placing != value:
+			placing = value
+			set_physics_process(placing)
+			
+			
+func _ready() -> void:
+	assert(target != null and target is Node3D, "Placeable3D: This placeable does not have a target Node3D assigned")
+
+	if origin_camera == null:
+		origin_camera = get_viewport().get_camera_3d()
+	
+	if placing:
+		placement_area.call_deferred("enable")
+	else:
+		placement_area.call_deferred("disable")
+		
+	set_process_unhandled_input(placing)
+	set_physics_process(placing)
---
+++ components/systems/build/placeable/placement_area_3d.gd
@@ -0,0 +1,40 @@
+@icon("res://components/systems/build/placeable/placement_area.svg")
+class_name PlacementArea3D extends Area3D
+
+enum PlacementType {
+	Building,
+	Prop,
+	Defense, 
+	Utility, 
+	Landscape, 
+	Workstation, 
+	PowerGrid, 
+	VehicleBay, 
+	Storage, 
+	Miscellaneous,
+	Module,
+	Trap,
+	LightSource
+}
+
+## Where this placement area belongs and can be detected by other areas
+@export_flags_3d_physics var placement_layers: int = 0
+## Other surfaces that can be detected
+@export_flags_3d_physics var surface_masks: int = 1
+@export var type: PlacementType = PlacementType.Building
+
+
+func _ready() -> void:
+	monitorable = true
+	monitoring = true
+	priority = 1
+	collision_layer = placement_layers
+	collision_mask = surface_masks
+	
+
+func enable() -> void:
+	show()
+
+
+func disable() -> void:
+	hide()
