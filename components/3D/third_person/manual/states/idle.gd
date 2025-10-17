c9d63cb4e40f03cadf6e27c7289bb1b1ead12fbf
sempitern0 <danieldigitalspace@proton.me>
2025-10-17 18:39:48 +0100

    improved clampf of camera angles on third person controller spring arm pivot

--- components/3D/third_person/manual/states/idle.gd
+++ components/3D/third_person/manual/states/idle.gd
@@ -3,9 +3,11 @@ class_name ThirdPersonIdleState extends ThirdPersonGroundState
 
 func physics_update(delta: float) -> void:
 	super.physics_update(delta)
-	decelerate(delta)
 	
-	if not actor.motion_input.input_direction.is_zero_approx():
-		FSM.change_state_to(ThirdPersonWalkState)
-	
+	if actor.is_on_floor():
+		decelerate(delta)
+		
+		if not actor.motion_input.input_direction.is_zero_approx():
+			FSM.change_state_to(ThirdPersonWalkState)
+		
 	actor.move_and_slide()
