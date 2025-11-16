877bba301d6bc940104268fb32f685c67b1d217e
sempitern0 <danieldigitalspace@proton.me>
2025-11-16 17:48:35 +0000

    improved the Planet3D script

--- components/3D/space/planets/planet.gd
+++ components/3D/space/planets/planet.gd
@@ -3,7 +3,6 @@ class_name Planet3D extends MeshInstance3D
 
 const GroupName: StringName = &"planets"
 
-@export_tool_button("Generate") var generate_planet: Callable = generate
 @export var configuration: PlanetConfiguration:
 	set(new_configuration_value):
 		_connect_configuration_listener(new_configuration_value)
@@ -16,24 +15,37 @@ func _enter_tree() -> void:
 	add_to_group(GroupName)
 	_connect_configuration_listener(configuration)
 	
+	update_parameters()
+	update_atmosphere()
 
-func generate() -> void:
-	pass
+
+func update_parameters() -> void:
+	if mesh:
+		mesh.radial_segments = configuration.resolution
+		mesh.rings = configuration.resolution
+		mesh.radius = configuration.radius
+		mesh.height = mesh.radius * 2.0
+		
+	if atmosphere and atmosphere.mesh:
+		atmosphere.mesh.radial_segments = configuration.resolution
+		atmosphere.mesh.rings = configuration.resolution
+		atmosphere.mesh.radius = configuration.radius
+		atmosphere.mesh.height = atmosphere.mesh.radius * 2.0
 
 
 func update_atmosphere() -> void:
-	if atmosphere and configuration.atmosphere:
-		atmosphere.process_mode = Node.PROCESS_MODE_PAUSABLE
-		atmosphere.show()
-	else:
-		atmosphere.process_mode = Node.PROCESS_MODE_DISABLED
-		atmosphere.hide()
-		
+	if atmosphere:
+		if configuration.atmosphere:
+			atmosphere.process_mode = Node.PROCESS_MODE_PAUSABLE
+			atmosphere.show()
+		else:
+			atmosphere.process_mode = Node.PROCESS_MODE_DISABLED
+			atmosphere.hide()
+
 		
 func _connect_configuration_listener(new_configuration: PlanetConfiguration = null) -> void:	
 	if Engine.is_editor_hint():
 		if new_configuration:
-			
 			if not new_configuration.changed.is_connected(on_configuration_changed):
 				new_configuration.changed.connect(on_configuration_changed)
 				
@@ -43,4 +55,5 @@ func _connect_configuration_listener(new_configuration: PlanetConfiguration = nu
 				
 
 func on_configuration_changed() -> void:
+	update_parameters()
 	update_atmosphere()
--- components/3D/space/planets/planet_configuration.gd
+++ components/3D/space/planets/planet_configuration.gd
@@ -1,11 +1,11 @@
 @tool
 class_name PlanetConfiguration extends Resource
 
-@export_range(2, 2048, 2) var resolution: int = 64:
+@export_range(2, 2048, 2) var resolution: int = 256:
 	set(value):
 		resolution = value
 		changed.emit()
-@export var radius: float = 100:
+@export var radius: float = 0.5:
 	set(value):
 		radius = value
 		changed.emit()
